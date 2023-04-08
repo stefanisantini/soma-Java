31e16634d4ca12b9387f722333b8b108b9daf17e
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-04-08 17:12:36 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,14 @@
+/**4)Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório 
+entre eles.
+Digite um valor: 8
+Digite outro valor: 5
+A soma entre 8 e 5 é igual a 13.*/
+public class Soma {
+    
+    public static void main (String [] args){
+        int n1 = 8;
+        int n2 = 5;
+        float n = n1 + n2;
+        System.out.println("A soma entre os número é igual a: " + n);
+    }
+}
